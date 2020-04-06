4bb722ebfe89ae5e7e8a182258141d72c6848c3d
PatharaNor <patharanor@gmail.com>
2020-04-06 09:15:38 +0700

    Develop (#12)
    * Dockerized and updated readme
    
    * Feature/admin page (#9)
    
    * Added frontend :
    - admin page (dashboard)
    - signin page
    - about page
    - optimized server site to serve client routes(nextjs pages)
    
    * Updated:
    - to support server site responding data object('query') to client via getInitialProps() method
    - changed auth callback endpoint from '/profile' to '/dashboard'
    - server provided signin page without checking auth
    - server provided middleware to check auth for secur page
    - added Facebook analytic
    - modified navigator to show current user who logged in to the service
    
    * Updated:
    - admin page
    - declare database schema
    - create endpoint for testing /api/v1/:name
    
    * Feature/admin page (#11)
    
    * Added frontend :
    - admin page (dashboard)
    - signin page
    - about page
    - optimized server site to serve client routes(nextjs pages)
    
    * Updated:
    - to support server site responding data object('query') to client via getInitialProps() method
    - changed auth callback endpoint from '/profile' to '/dashboard'
    - server provided signin page without checking auth
    - server provided middleware to check auth for secur page
    - added Facebook analytic
    - modified navigator to show current user who logged in to the service
    
    * Updated:
    - admin page
    - declare database schema
    - create endpoint for testing /api/v1/:name
    
    * Fixed component not found issue

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,6 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import MuiLink from '@material-ui/core/Link';
-import Button from '@material-ui/core/Button';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
 
 import Copyright from '../components/Copyright';
 
@@ -16,10 +12,6 @@ export default function About() {
         <Typography variant="h4" component="h1" gutterBottom>
           About PatharaNor
         </Typography>
-        <Button variant="contained" color="primary" component={Link} naked href="/">
-          Go to the main page
-        </Button>
-        <ProTip />
         <Copyright />
       </Box>
     </Container>
