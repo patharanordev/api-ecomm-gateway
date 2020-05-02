0fd1cd013cef2fc0bfc516eb78bd0e12c07a2415
patharanor <patharanor@gmail.com>
2020-05-02 21:00:01 +0700

    Shown waiting component when updated order item status

--- pages/order.js
+++ pages/order.js
@@ -2,6 +2,7 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import { connect } from 'react-redux';
 import RESTFul from '../helper/RESTFul';
+import Loading from '../components/Loading';
 
 const MenuComponent = dynamic( import('../components/menu/Menu'), { ssr: false } )
 const OrderComponent = dynamic( import('../components/order/OrderTable'), { ssr: false } )
@@ -21,35 +22,46 @@ class Order extends React.Component {
     super(props)
 
     this.state = {
-      orderItems: null
+      orderItems: null,
+      isWaiting: true
     }
   }
 
   getOrderItems() {
-    const url = `/api/v1/payment`;
-    const data = { "method":"order", "type":"list", "options": { "order": [ ["timestamp", "DESC"] ] } }
-    rFul.post(url, data, (err, data) => {
-      if(err) { console.log('Select order error : ', err); } 
-      else {
-        console.log('Get order status success : ', data);
-        this.setState({ orderItems:data })
-      }
+    this.setState({ isWaiting:true }, () => {
+      const url = `/api/v1/payment`;
+      const data = { "method":"order", "type":"list", "options": { "order": [ ["timestamp", "DESC"] ] } }
+      rFul.post(url, data, (err, data) => {
+        this.setState({ isWaiting:false }, () => {
+          if(err) { console.log('Select order error : ', err); } 
+          else {
+            console.log('Get order status success : ', data);
+            this.setState({ orderItems:data })
+          }
+        });
+      });
     });
   }
 
   updateOrderItemStatus(e, record_id, callback) {
     if(record_id) {
-      rFul.post(`/api/v1/payment`, { 
-        "method":"update", 
-        "update": {
-          "condition": { record_id:record_id },
-          "data": { order_status:e.target.value },
-        }
-      }, (err, data) => {
-        if(typeof callback === 'function') {
-          callback(err, data);
-        }
-      });
+      this.setState({ isWaiting:true }, () => {
+        rFul.post(`/api/v1/payment`, { 
+          "method":"update", 
+          "update": {
+            "condition": { record_id:record_id },
+            "data": { order_status:e.target.value },
+          }
+        }, (err, data) => {
+
+          this.setState({ isWaiting:false }, () => {
+            if(typeof callback === 'function') {
+              callback(err, data);
+            }
+          })
+
+        });
+      })
     }
   }
 
@@ -59,26 +71,30 @@ class Order extends React.Component {
 
   render() {
     const { user } = this.props;
-    const { orderItems } = this.state;
+    const { orderItems, isWaiting } = this.state;
 
     if(MenuComponent) {
       return (
         <MenuComponent currentUser={user && user.currentUser ? user.currentUser : tmpUser} title='Order'>
         {
-          OrderComponent
-          ? 
-            <OrderComponent 
-              orderItems={orderItems} 
-              onStatusChange={(e, record_id)=> {
-                // Update order status
-                this.updateOrderItemStatus(e, record_id, () => {
-                  // Refresh order item table
-                  this.getOrderItems()
-                })
-              }}
-            />
-          : 
-            null
+          isWaiting==false
+          ?
+            OrderComponent
+            ? 
+              <OrderComponent 
+                orderItems={orderItems} 
+                onStatusChange={(e, record_id)=> {
+                  // Update order status
+                  this.updateOrderItemStatus(e, record_id, () => {
+                    // Refresh order item table
+                    this.getOrderItems()
+                  })
+                }}
+              />
+            : 
+              null
+          :
+            <Loading />
         }
         </MenuComponent>
       )
